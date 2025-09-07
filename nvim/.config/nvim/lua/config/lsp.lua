b9779d4af08a0f0d8c3c577bf4ca5af8e9a3230b
Jorge Estrada <jor1550g@gmail.com>
2025-09-07 18:39:57 -0500

    refactor: set lsp picker to telescope

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -7,15 +7,29 @@ local lsp_attach = function(_, bufnr)
   local opts = { buffer = bufnr }
 
   vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-  vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+  -- vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
   vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-  vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-  vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-  vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+  -- vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+  -- vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+  -- vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
   vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
   vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
   vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
   vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+
+  -- ==================
+  -- lsp with telescope
+  -- ==================
+  local builtin = require("telescope.builtin")
+
+  -- Definitions
+  vim.keymap.set("n", "gd", builtin.lsp_definitions, opts)
+  -- Implementations
+  vim.keymap.set("n", "gi", builtin.lsp_implementations, opts)
+  -- Type Definition
+  vim.keymap.set("n", "go", builtin.lsp_type_definitions, opts)
+  -- References
+  vim.keymap.set("n", "gr", builtin.lsp_references, opts)
 end
 
 lsp_zero.extend_lspconfig({
