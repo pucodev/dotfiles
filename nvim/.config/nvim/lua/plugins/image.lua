9c6ee881689c230626f3e00b2eb3aba72eedd382
Jorge Estrada <jor1550g@gmail.com>
2025-10-05 20:52:50 -0500

    feat: add image to render images in explorer and md files

---
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -0,0 +1,8 @@
+-- First need install ImageMagick (https://github.com/3rd/image.nvim?tab=readme-ov-file#imagemagick)
+return {
+  "3rd/image.nvim",
+  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  opts = {
+    processor = "magick_cli",
+  },
+}
