cfb7052b9d33bb4fd7673e1e1d2266233383d5d5
Jorge Estrada <jor1550g@gmail.com>
2026-03-10 17:23:38 -0500

    (nvim) add: lua and vue lsp

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -58,6 +58,7 @@ require("mason-lspconfig").setup({
     "intelephense",
     "lua_ls",
     "mdx_analyzer",
+    "vue_ls",
     "pyright",
     "somesass_ls",
     "vtsls",
@@ -68,70 +69,101 @@ require("mason-lspconfig").setup({
 -- CONFIG LSP
 -- -------------------
 
+-- Lua
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+
 -- MDX
-local npm = require("util.npm")
-local mdx_plugin = "@mdx-js/typescript-plugin"
-npm.ensure_npm_binary(mdx_plugin, {
-  on_success = function()
-    local mdx_lsp_path = npm.get_package_path() .. "/" .. mdx_plugin
-    vim.lsp.config("vtsls", {
-      settings = {
-        vtsls = {
-          autoUseWorkspaceTsdk = true,
-          tsserver = {
-            globalPlugins = {
-              {
-                name = "@mdx-js/typescript-plugin",
-                enableForWorkspaceTypeScriptVersions = true,
-                location = mdx_lsp_path,
-                languages = {
-                  "mdx",
-                },
-              },
-            },
-          },
+-- local npm = require("util.npm")
+-- local mdx_plugin = "@mdx-js/typescript-plugin"
+-- npm.ensure_npm_binary(mdx_plugin, {
+--   on_success = function()
+--     local mdx_lsp_path = npm.get_package_path() .. "/" .. mdx_plugin
+--     vim.lsp.config("vtsls", {
+--       settings = {
+--         vtsls = {
+--           autoUseWorkspaceTsdk = true,
+--           tsserver = {
+--             globalPlugins = {
+--               {
+--                 name = "@mdx-js/typescript-plugin",
+--                 enableForWorkspaceTypeScriptVersions = true,
+--                 location = mdx_lsp_path,
+--                 languages = {
+--                   "mdx",
+--                 },
+--               },
+--             },
+--           },
+--         },
+--       },
+--       filetypes = {
+--         "javascript",
+--         "javascriptreact",
+--         "javascript.jsx",
+--         "typescript",
+--         "typescriptreact",
+--         "typescript.tsx",
+--         "mdx",
+--       },
+--     })
+--   end,
+-- })
+
+-- Vue
+local vue_language_server_path = vim.fn.stdpath("data")
+  .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+local vue_plugin = {
+  name = "@vue/typescript-plugin",
+  location = vue_language_server_path,
+  languages = { "vue" },
+  configNamespace = "typescript",
+}
+local vtsls_config = {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin,
         },
       },
-      filetypes = {
-        "javascript",
-        "javascriptreact",
-        "javascript.jsx",
-        "typescript",
-        "typescriptreact",
-        "typescript.tsx",
-        "mdx",
-      },
-    })
-    -- require("lspconfig").vtsls.setup({
-    --   settings = {
-    --     vtsls = {
-    --       autoUseWorkspaceTsdk = true,
-    --       tsserver = {
-    --         globalPlugins = {
-    --           {
-    --             name = "@mdx-js/typescript-plugin",
-    --             enableForWorkspaceTypeScriptVersions = true,
-    --             location = mdx_lsp_path,
-    --             languages = {
-    --               "mdx",
-    --             },
-    --           },
-    --         },
-    --       },
-    --     },
-    --   },
-    --   filetypes = {
-    --     "javascript",
-    --     "javascriptreact",
-    --     "javascript.jsx",
-    --     "typescript",
-    --     "typescriptreact",
-    --     "typescript.tsx",
-    --     "mdx",
-    --   },
-    -- })
-  end,
-})
+    },
+  },
+  filetypes = tsserver_filetypes,
+}
+
+local ts_ls_config = {
+  init_options = {
+    plugins = {
+      vue_plugin,
+    },
+  },
+  filetypes = tsserver_filetypes,
+}
+
+local vue_ls_config = {}
+vim.lsp.config("vtsls", vtsls_config)
+vim.lsp.config("vue_ls", vue_ls_config)
+vim.lsp.config("ts_ls", ts_ls_config)
+vim.lsp.enable({ "vtsls", "vue_ls" })
 
 -- EMMET
 -- add md and mdx to filetypes
